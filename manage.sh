72266417b0dd29ae5d3106247c0993b0e9c67ec7
Lord Turmoil <tony-turmoil@outlook.com>
2024-07-06 10:48:11 +0800

    update: more help text in manage.sh

--- manage.sh
+++ manage.sh
@@ -1,50 +1,75 @@
+# check if the value is "a" or "apply"
+function is_apply() {
+    if [ "$1" == "-a" ] || [ "$1" == "--apply" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function is_delete() {
+    if [ "$1" == "-d" ] || [ "$1" == "--delete" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function apply_usage() {
+    echo "Usage: $0 $1 [-a|--apply|-d|--delete]"
+}
+
 if [ "$1" == "nginx" ]; then
+    if [ ! -z "$2" ]; then
+        echo "Usage: $0 nginx"
+        exit 1
+    fi
     sudo cp patpat.conf /etc/nginx/conf.d/patpat.conf
     sudo nginx -s reload
 elif [ "$1" == "env" ]; then
-    if [ "$2" == "a" ]; then
+    if is_apply $2; then
         sudo kubectl apply -f env.yaml
-    elif [ "$2" == "d" ]; then
+    elif is_delete $2; then
         sudo kubectl delete -f env.yaml
     else
-        echo "Usage: $0 env [a|d]"
+        apply_usage $1 $2
     fi
 elif [ "$1" == "service" ]; then
-    if [ "$2" == "a" ]; then
+    if is_apply $2; then
         sudo kubectl apply -f service.yaml
-    elif [ "$2" == "d" ]; then
+    elif is_delete $2; then
         sudo kubectl delete -f service.yaml
     else
-        echo "Usage: $0 service [a|d]"
+        apply_usage $1 $2
     fi
 elif [ "$1" == "judge" ]; then
-    if [ "$2" == "a" ]; then
+    if is_apply $2; then
         sudo kubectl apply -f judge/judge.yaml
-    elif [ "$2" == "d" ]; then
+    elif is_delete $2; then
         sudo kubectl delete -f judge/judge.yaml
     else
-        echo "Usage: $0 judge [a|d]"
+        apply_usage $1 $2
     fi
 elif [ "$1" == "boot" ]; then
-    if [ "$2" == "a" ]; then
+    if is_apply $2; then
         sudo kubectl apply -f boot/boot.yaml
-    elif [ "$2" == "d" ]; then
+    elif is_delete $2; then
         sudo kubectl delete -f boot/boot.yaml
     else
-        echo "Usage: $0 boot [a|d]"
+        apply_usage $1 $2
     fi
 elif [ "$1" == "stop" ]; then
     sudo kubectl delete -f boot/boot.yaml
     sudo kubectl delete -f judge/judge.yaml
 elif [ "$1" == "watch" ]; then
-    if [ "$2" == "d" ]; then
+    if [ "$2" == "-d" ] || [ "$2" == "--deployment" ]; then
         sudo kubectl get deployment --watch
-    elif [ "$2" == "p" ]; then
+    elif [ "$2" == "-p" ] || [ "$2" == "--pod" ]; then
         sudo kubectl get pod --watch
-    elif [ "$2" == "s" ]; then
+    elif [ "$2" == "-s" ] || [ "$2" == "--service" ]; then
         sudo kubectl get service --watch
     else
-        echo "Usage: $0 watch [d|p|s]"
+        echo "Usage: $0 watch [-d|--deployment|-p|--pod|-s|--service]"
     fi
 elif [ "$1" == "log" ]; then
     if [ "$2" == "--clear" ]; then
@@ -57,19 +82,26 @@ elif [ "$1" == "log" ]; then
         echo "Usage: $0 log [--clear|boot|judge]"
     fi
 elif [ "$1" == "deploy" ]; then
-    ./manage.sh env a
-    if [ "$2" == "--clear" ]; then
-        ./manage.sh log --clear
-    elif [ "$2" == "all" ]; then
-        cd boot && ./deploy.sh && cd ..
-        cd judge && ./deploy.sh && cd ..
+    ./manage.sh env apply
+    if [ "$2" == "-a" ] || [ "$2" == "--all" ]; then
+        ./deploy.sh boot
+        ./deploy.sh judge
     elif [ "$2" == "boot" ]; then
-        cd boot && ./deploy.sh && cd ..
+        ./deploy.sh boot
     elif [ "$2" == "judge" ]; then
-        cd judge && ./deploy.sh && cd ..
+        ./deploy.sh judge
     else
-        echo "Usage: $0 deploy [--clear|all|boot|judge]"
+        echo "Usage: $0 deploy [-a|--all|boot|judge]"
     fi
 else
-    echo "Usage: $0 [nginx|env|service|judge|boot|stop|watch]"
+    echo "Usage: $0 verb [options]"
+    echo "  nginx    --  reload nginx configuration"
+    echo "  env      --  apply environment variables"
+    echo "  service  --  apply service"
+    echo "  judge    --  manage judge application"
+    echo "  boot     --  manage boot application"
+    echo "  stop     --  stop all applications"
+    echo "  watch    --  watch deployment, pod, or service"
+    echo "  log      --  manage log files"
+    echo "  deploy   --  deploy applications"
 fi
