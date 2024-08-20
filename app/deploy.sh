10522a121ea1ce52e3c635c88bbc5ad471037ff1
Lord Turmoil <tony-turmoil@outlook.com>
2024-08-20 14:20:34 +0800

    update: tutorial deployment

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
+
 echo "===== `date`" | tee -a deploy.log
 
-echo "[INFO] Checking dist/ directory" | tee -a deploy.log
-if [ ! -d "dist" ]; then
-    echo "[ERROR] dist/ directory not found" | tee -a deploy.log
+if [ -z "$1" ]; then
+    echo "[ERROR] Missing source directory" | tee -a deploy.log
+    echo "" | tee -a deploy.log
+    exit 1
+fi
+if [ -z "$2" ]; then
+    echo "[ERROR] Missing destination directory" | tee -a deploy.log
+    echo "" | tee -a deploy.log
     exit 1
 fi
 
-echo "[WARNING] Cleaning up old deployment" | tee -a deploy.log
-rm -rf patpat
+source_dir=$1
+dest_dir=$2
+
+echo "[INFO] Checking existence of '$source_dir'" | tee -a deploy.log
+if [ ! -d "$source_dir" ]; then
+    echo "[ERROR] Directory '$source_dir' not found" | tee -a deploy.log
+    exit 1
+fi
+
+echo "[WARNING] Cleaning up old deployment '$dest_dir'" | tee -a deploy.log
+rm -rf "$dest_dir"
 
 echo "[INFO] Deploying new version" | tee -a deploy.log
-mv dist patpat
+mv "$source_dir" "$dest_dir"
 
 echo "[INFO] Deployment successful" | tee -a deploy.log
 echo "" | tee -a deploy.log
