4f242314c426d17dbb72a514cc3e64b6ba39a002
Lord Turmoil <tony-turmoil@outlook.com>
2024-09-11 23:31:43 +0800

    feat: dark theme and layout for game

--- app/game/js/application.js
+++ app/game/js/application.js
@@ -2,3 +2,25 @@
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
+
+function switch_theme() {
+  let mode = localStorage.getItem('color-mode') || 'light';
+  if (mode === 'dark') {
+    let dark = document.createElement('link');
+    dark.rel = 'stylesheet';
+    dark.type = 'text/css';
+    dark.href = 'style/dark.css';
+    dark.id = 'dark-mode';
+    document.head.appendChild(dark);
+  } else {
+    let dark = document.getElementById('dark-mode');
+    if (dark) {
+      dark.remove();
+    }
+  }
+}
+
+switch_theme();
+
+// watch for changes in the color-mode
+window.addEventListener('storage', switch_theme);
